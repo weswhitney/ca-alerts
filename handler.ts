c7c521c31e99a9e71c0b51f510e557465340d3e7
weswhitney <wesleywhitney@gmail.com>
2022-05-15 20:26:33 -0600

    feat: working correctly

--- handler.ts
+++ handler.ts
@@ -1,25 +1,7 @@
 require("dotenv").config()
 import forecast from "./lib/forecast"
 
-import AWS from "aws-sdk"
-
-module.exports.run = async (
-  _event: any,
-  context: { done: any },
-  _callback: (arg0: null, arg1: { success: boolean }) => void
-) => {
-  const forecastResponse = await forecast()
-  let msg
-  forecastResponse.alerts[0] !== undefined
-    ? (msg = forecastResponse.alerts[0].description)
-    : (msg = "no alerts at this time")
-  const sns = new AWS.SNS()
-  const params = {
-    Message: msg,
-    Subject: "Usage Alert from Colorado Aware",
-    TopicArn: process.env.TOPIC_ARN,
-  }
-  console.log("params ", params)
-  console.log("sns ", sns.config.credentials)
-  sns.publish(params, context.done)
+module.exports.run = async (event: any, context: any, callback: any) => {
+  const result = forecast()
+  callback(null, result)
 }
--- lib/forecast.ts
+++ lib/forecast.ts
@@ -1,10 +1,26 @@
 import fetch from "node-fetch"
+import AWS from "aws-sdk"
+
 let url = `https://api.weatherbit.io/v2.0/alerts?lat=39.761554&lon=-104.962095&key=${process.env.WEATHER_BIT_API_KEY}`
 
 const forecast = async function () {
   const response = await fetch(url)
-  const body = await response.json()
-  return body
+  const forecastResponse = await response.json()
+
+  let msg
+  forecastResponse.alerts[0] !== undefined
+    ? (msg = forecastResponse.alerts[0].description)
+    : (msg = "no alerts at this time")
+  const sns = new AWS.SNS()
+  const params = {
+    Message: msg,
+    Subject: "Usage Alert from Colorado Aware",
+    TopicArn: process.env.TOPIC_ARN,
+  }
+  sns.publish(params, function (err, data) {
+    if (err) console.log("error in send message", err)
+    else console.log(data)
+  })
 }
 
 export default forecast
